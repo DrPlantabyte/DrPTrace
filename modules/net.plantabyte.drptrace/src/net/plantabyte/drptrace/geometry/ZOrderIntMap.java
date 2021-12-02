0d7a21b79f15c146b0a5073ffc134b0f6764d117
DrPlantabyte <explosivegnome@yahoo.com>
2021-12-02 11:52:07 +1100

    wip

--- modules/net.plantabyte.drptrace/src/net/plantabyte/drptrace/geometry/ZOrderIntMap.java
+++ modules/net.plantabyte.drptrace/src/net/plantabyte/drptrace/geometry/ZOrderIntMap.java
@@ -8,37 +8,36 @@ public class ZOrderIntMap extends IntMap{
 	public ZOrderIntMap(int width, int height){
 		this.width = width;
 		this.height = height;
-		final int size = 256 * (width >> 4 + 1) * (height >> 4 + 1);
+		final int size = 256 * ((width >> 4) + 1) * ((height >> 4) + 1);
 		this.data = new int[size];
 	}
-	private static final int[] ZLUT = {
-			0b00000000,
-			0b00000001,
-			0b00000100,
-			0b00000101,
-			0b00010000,
-			0b00010001,
-			0b00010100,
-			0b00010101,
-			0b01000000,
-			0b01000001,
-			0b01000100,
-			0b01000101,
-			0b01010000,
-			0b01010001,
-			0b01010100,
-			0b01010101
-	};
+	
 	private static int zorder4bto8it(final int x, final int y){
+		final byte[] ZLUT = {
+				0b00000000,
+				0b00000001,
+				0b00000100,
+				0b00000101,
+				0b00010000,
+				0b00010001,
+				0b00010100,
+				0b00010101,
+				0b01000000,
+				0b01000001,
+				0b01000100,
+				0b01000101,
+				0b01010000,
+				0b01010001,
+				0b01010100,
+				0b01010101
+		};
 		final int xBits = ZLUT[x & 0x0F];
 		final int yBits = ZLUT[y & 0x0F] << 1;
 		return xBits | yBits;
 	}
 	private int index(final int x, final int y){
-		final int subW = this.width >>> 4;
-		final int subX = x >>> 4;
-		final int subY = y >>> 4;
-		return ((subY*subW+subX) << 8) | zorder4bto8it(x, y);
+		final int i = width * y + x;
+		return (i << 8) | zorder4bto8it(x, y);
 	}
 	
 	
@@ -79,14 +78,14 @@ public class ZOrderIntMap extends IntMap{
 		var b = new ZOrderIntMap(w, h);
 		for(int y = 0; y < b.getHeight(); y++) {
 			for(int x = 0; x < b.getWidth(); x++) {
-				if(Math.sqrt(x*x+y*y) < 50){
+				if(Math.sqrt(x*x+y*y) < h){
 					b.set(x, y, x*x+y*y);
 				}
 			}
 		}
 		for(int y = 0; y < b.getHeight(); y++) {
 			for(int x = 0; x < b.getWidth(); x++) {
-				if(Math.sqrt(x*x+y*y) < 50){
+				if(Math.sqrt(x*x+y*y) < h){
 					if(b.get(x, y) != x*x+y*y) {
 						throw new RuntimeException("FUCK!");
 					}
